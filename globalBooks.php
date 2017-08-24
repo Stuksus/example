0c8df7bbfac3dc86295ad2bbe3e6ca356ea47676
Stuksus <itsoluution@gmail.com>
2017-08-24 11:33:05 +0300

    Add files via upload

--- globalBooks.php
+++ globalBooks.php
@@ -1,4 +1,29 @@
-<?php
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>Библиотека</h1>
+<table border="1">
+    <tr>
+        <td>
+            Название
+        </td>
+        <td>
+            Автор
+        </td>
+        <td>
+            Дата
+        </td>
+        <td>
+            Жанр
+        </td>
+    </tr>
+    <?php
 
 
     $bd = mysqli_connect("university.netology.ru", "smetanin", "neto1247", "global");
@@ -6,14 +31,18 @@
     $sql = mysqli_query($bd, $res);
 
 
-   while ($data = mysqli_fetch_array($sql)):
-       echo
-       "<tr><td>",$data['name'],
-       "</td><td>",$data['author'],
-       "</td><td>",$data['year'],
-       "</td><td>",$data['isbn'],
-       "</td><td>",$data['genre'],
-       "</td></tr>"
-       ;
-   endwhile;
-   ?>
+    while ($data = mysqli_fetch_array($sql)):
+        echo
+        "<tr><td>",$data['name'],
+        "</td><td>",$data['author'],
+        "</td><td>",$data['year'],
+        "</td><td>",$data['isbn'],
+        "</td><td>",$data['genre'],
+        "</td></tr>"
+        ;
+    endwhile;
+    ?>
+</table>
+</body>
+</html>
+
